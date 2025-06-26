df2163170c89c866c138f431b0e7a3ecb2fc9a35
Anjana-Varghese <anjanasini03@gmail.com>
2025-06-27 00:01:39 +0530

    initial commit with readme and project files

---
+++ main.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from sklearn import linear_model
+import matplotlib.pyplot as plt
+
+# Read data
+dataframe = pd.read_fwf('brain_body.txt')
+x_values = dataframe[['Brain']]
+y_values = dataframe[['Body']]
+
+# Train model
+body_reg = linear_model.LinearRegression()
+body_reg.fit(x_values, y_values)
+
+# Print equation
+print("Regression Equation: y =", round(body_reg.coef_[0][0], 2), "* x +", round(body_reg.intercept_[0], 2))
+
+# Predict
+brain_input = float(input("Enter brain weight to predict body weight: "))
+predicted_body = body_reg.predict([[brain_input]])
+print("Predicted body weight:", round(predicted_body[0][0], 2))
+
+# Plot
+plt.scatter(x_values, y_values)
+plt.plot(x_values, body_reg.predict(x_values))
+plt.show()
